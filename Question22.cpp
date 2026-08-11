2368a8b3ec6c18684880051d84e944d115e82619
zoyaasif <zoya021asif@gmail.com>
2026-08-12 02:50:26 +0530

    Questions.

---
+++ Question22.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            cout<<" ";
+        }
+        char ch = 'A';
+        int breakpoint = (2*i+1)/2;
+        for(int j = 1; j <= 2*i+1; j++){
+            cout<<ch;
+            if(j <= breakpoint) ch++;
+            else ch--;
+        }
+        for(int j = 0; j < n - i + 1; j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
