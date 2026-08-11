6e1722731f11cf7ee09f9825da0edc494665e363
zoyaasif <zoya021asif@gmail.com>
2026-08-12 02:57:19 +0530

    Question.

---
+++ Question23.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    for(int i = 0; i < n; i++){
+        for(char ch = 'E'-i;ch <= 'E'; ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
