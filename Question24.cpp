910b92997f870c022b987e30e038de5c07bf85b6
zoyaasif <zoya021asif@gmail.com>
2026-08-21 15:15:33 +0530

    Count of numbers.

---
+++ Question24.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter a number:";
+    cin >> n;
+
+    int cnt = 0;
+
+    while (n > 0) {
+        n = n / 10;
+        cnt++;
+    }
+
+    cout << "Number of digits = " << cnt;
+
+    return 0;
+}
