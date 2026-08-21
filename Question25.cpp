ff3de58b7a7a4a6459ca6f8c8f91a232461e307b
zoyaasif <zoya021asif@gmail.com>
2026-08-21 18:59:42 +0530

    Pallindrome number.

--- Question25.cpp
+++ Question25.cpp
@@ -5,12 +5,15 @@ int main(){
     int n;
     cout<<"Enter a number:";
     cin>>n;
+     int dup = n;
     int revnum = 0;
     while(n > 0){
         int ld = n % 10;
         revnum = (revnum*10) + ld;
         n = n / 10;
     }
-    cout<<revnum;
+    if(dup == revnum)
+       cout<<"true";
+    else cout <<"false";
     return 0;
 }
