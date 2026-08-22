cb3f0ea0afb6c6c3b440e8beb48c63bafd9bf230
zoyaasif <zoya021asif@gmail.com>
2026-08-22 14:27:47 +0530

    Print all divisors.

---
+++ Question27.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+
+    for(int i = 1; i <= n; i++){
+        if(n % i == 0){
+            cout<< i << " ";
+
+        }
+    }
+    return 0;
+}
