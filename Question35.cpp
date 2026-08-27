8a428d423a61f6c5d0485027b7af37173ebc2bee
zoyaasif <zoya021asif@gmail.com>
2026-08-28 01:09:54 +0530

    Print 1 to n by backtrack.

---
+++ Question35.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void printNumbers(int i ){
+    if ( i < 1 ){
+       return;
+    }
+    printNumbers(i - 1);
+    cout<< i << " ";
+    
+}
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    printNumbers(n);
+    return 0;
+}
