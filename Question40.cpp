11df9ec018f159499e946ce01d1f24b06c53cbb2
zoyaasif <zoya021asif@gmail.com>
2026-09-01 19:13:36 +0530

    Reverse a number.

---
+++ Question40.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;;
+    cout<<"Enter a number:";
+    cin>>n;
+    int revnum = 0;
+    while(n > 0){
+        int lastdigit = n % 10;
+        revnum = (revnum * 10) + lastdigit;
+        n = n / 10;
+
+    }
+    cout<<revnum<<endl;
+    return 0;
+
+}
