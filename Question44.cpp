8bf9f7e26d323d0e552d416d0b1f57d0c056c80f
zoyaasif <zoya021asif@gmail.com>
2026-09-02 00:03:53 +0530

    Armstrong number.

---
+++ Question44.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    int dup = n;
+    int sum = 0;
+    while(n > 0){
+        int lastdigit = n % 10;
+        sum = sum + ( lastdigit * lastdigit * lastdigit);
+        n = n / 10;
+    }
+    if(dup == sum){
+        cout<<"Armstrong number"<<endl;
+    }
+    else{
+        cout<<"Not an Armstrong number"<<endl;
+    }
+    return 0;
+}
