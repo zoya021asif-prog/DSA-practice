5ddfac1e184b35dbdf609f35cef253bba863ea04
zoyaasif <zoya021asif@gmail.com>
2026-09-15 20:47:55 +0530

    Question.

--- Question58.cpp
+++ Question58.cpp
@@ -14,6 +14,9 @@ int main(){
     for(int i = 0; i < n; i++){
         mpp[arr[i]]++;
     }
+    for(auto it : mpp){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
     int q;
     cout<<"Enter the number of queries:";
     cin>>q;
