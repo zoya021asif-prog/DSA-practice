9b611fce33300d2cbaff6caeb29185fd7afb2726
zoyaasif <zoya021asif@gmail.com>
2026-09-15 20:42:41 +0530

    Question.

---
+++ Question58.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    //precompute
+    map <int,  int> mpp;
+    for(int i = 0; i < n; i++){
+        mpp[arr[i]]++;
+    }
+    int q;
+    cout<<"Enter the number of queries:";
+    cin>>q;
+    while(q--){
+        int number;
+        cout<<"Enter the number to fetch frequency:";
+        cin>> number;
+        //fetch
+        cout<<mpp[number]<<endl;
+    }
+    return 0;
+}
