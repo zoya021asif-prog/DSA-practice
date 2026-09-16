8a679116ce1e7835e28ce543bf5e215190e1d52d
zoyaasif <zoya021asif@gmail.com>
2026-09-16 21:05:11 +0530

    Print n to 1.

---
+++ Question61.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void printName(int i, int n){
+    if (i < n){
+        return;
+    }
+    cout<< i <<endl;
+    printName(i - 1, n);
+}
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    printName(n,1);
+}
