8a1bb45cf3ffca98528573373319c8e745649702
zoyaasif <zoya021asif@gmail.com>
2026-09-17 23:54:44 +0530

    Selection sort.

---
+++ Question65.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void selection_sort(int arr[], int n){
+    for(int i = 0; i < n - 1; i++){
+        int mini = i;
+        for(int j = i; j <= n - 1; j++){
+            if(arr[j] < arr[mini]){
+                mini = j;
+            }
+        }
+        int temp = arr[i];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array:";
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++)cin>>arr[i];
+    selection_sort(arr,n);
+    for(int i = 0; i < n; i++)cout<<arr[i]<<" ";
+    return 0;
+    
+}
